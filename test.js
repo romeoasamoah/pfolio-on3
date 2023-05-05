97497d56e4e4c21340b618f249da9416be11f8a9
romeoasamoah <93823969+romeoasamoah@users.noreply.github.com>
2023-05-05 10:01:01 +0000

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+var clothes = [];
+  clothes.push('gray t-shirt');
+  clothes.push('green scarf');
+  clothes.pop();
+  clothes.push('slippers');
+  clothes.pop();
+  clothes.push('boots');
+  clothes.push('old jeans');
+  console.log(clothes)
